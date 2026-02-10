63b5140bd7cd1b8b1f3f5a4c4142e4656460c920
Andres Gaviria <cgaviria930@gmail.com>
2026-02-09 23:46:44 -0500

    cambios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <div className='animate-slide-in-top fixed w-full py-4 md:py-0 top-5 left-1/2 -translate-x-1/2 flex justify-center gap-x-4 md:gap-x-10 items-center md:px-6 tracking-wide md:tracking-wider font-semibold text-white z-50 bg-white/50 backdrop-blur-xl'>
+        <div className='animate-slide-in-top fixed w-full py-4 md:py-0 top-5 left-1/2 -translate-x-1/2 flex justify-center gap-x-4 md:gap-x-10 items-center md:px-6 tracking-wide md:tracking-wider font-semibold text-white z-50 bg-white/60 backdrop-blur-md'>
             <a href="#" className='text-center py-3 hover:scale-110 hover:duration-300 hover:text-purple-800'>Quien Soy</a>
             <a href="#Recorrido" className='text-center py-3 hover:scale-110 hover:duration-300 hover:text-purple-800'>Recorrido</a>
             <a href="#Propuestas" className='text-center py-3 hover:scale-110 hover:duration-300 hover:text-purple-800'>Propuestas</a>
